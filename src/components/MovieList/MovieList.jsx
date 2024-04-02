b71e7588afd0b5178c30ec84ac3e336bb0c77ac2
IgorGra1 <gis1@ukr.net>
2024-04-03 00:14:32 +0300

    update

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,42 @@
-const MovieList = () => {
+import { Link } from "react-router-dom";
+
+const MovieList = ({ movies }) => {
   return (
-    <ul>
-      {/* {movies.map((movie) => (
-        <li key={movie.id}></li>
-      ))} */}
-    </ul>
+    <div>
+      <ul>
+        {Array.isArray(movies) &&
+          movies.map((movie) => (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`}>
+                <p>{movie.title}</p>
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
   );
 };
 
 export default MovieList;
+
+// adult: false;
+// backdrop_path: "/sR0SpCrXamlIkYMdfz83sFn5JS6.jpg";
+// genre_ids: (3)[(28, 878, 12)];
+// id: 823464;
+// media_type: "movie";
+// original_language: "en";
+// original_title: "Godzilla x Kong: The New Empire";
+// overview: "Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered threat hidden within our world, challenging their very existence – and our own.";
+// popularity: 5072.084;
+// poster_path: "/iG5sbWWK1JEPAxdt3ItsCMGGV4p.jpg";
+// release_date: "2024-03-27";
+// title: "Godzilla x Kong: The New Empire";
+// video: false;
+// vote_average: 6.836;
+// vote_count: 271;
+{
+  /* <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+          /> */
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,42 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import MovieList from "../components/MovieList/MovieList";
+// import { Outlet } from "react-router-dom";
 
 const HomePage = () => {
+  const [movies, setMovies] = useState(null);
+
+  useEffect(() => {
+    async function fetchTrendingMovies() {
+      const options = {
+        method: "GET",
+        url: "https://api.themoviedb.org/3/trending/movie/day",
+        params: { language: "en-US" },
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NTJjOTRmNWZhNWVmNTMxY2M5ZGZhYTBhOTYwZmYxNyIsInN1YiI6IjY2MDkyNzNhZDRmZTA0MDE3YzJhMzc2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SEkEt_-LzYjq6JSyF--2DSj0F8tnxODl0Pfw1-3Qfls",
+        },
+      };
+
+      axios
+        .request(options)
+        .then(function (response) {
+          const trendingMovies = response.data.results;
+          console.log(trendingMovies);
+          setMovies(trendingMovies);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
+    fetchTrendingMovies();
+  }, []);
   return (
     <div>
-      <p>Trending today</p>
-      <MovieList />
+      <h1>Trending today</h1>
+      {movies !== null && <MovieList movies={movies} />}
+      {/* <Outlet /> */}
     </div>
   );
 };
