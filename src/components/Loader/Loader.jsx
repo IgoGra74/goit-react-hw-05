a73d8568db459f2eb47467d832c1526cefae75af
IgorGra1 <gis1@ukr.net>
2024-04-07 00:46:19 +0300

    update

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,8 @@
 import { ThreeDots } from "react-loader-spinner";
-import css from "./Loader.module.css";
 
 const Loader = () => {
   return (
-    <div className={css.loading}>
+    <div>
       <ThreeDots
         visible={true}
         height="80"
